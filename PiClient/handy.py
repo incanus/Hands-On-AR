3050413bb57451da356276cc685bb326f66069e7
Justin R. Miller <incanus@codesorcery.net>
2018-09-13 17:23:38 +0000

    tweaks to flex sensitivity

--- PiClient/handy.py
+++ PiClient/handy.py
@@ -16,15 +16,13 @@ mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 lsm = LSM6DS3()
 
-VCC = 3.3
-RDIV_THUMB = 47000
+VCC = 3.316
+RDIV_THUMB = 47100
 RDIV_FINGER = 10000
-STRAIGHT_THUMB = 38000
-STRAIGHT_FINGER = 10000
-BEND_THUMB = 110000
-BEND_FINGER = 20000
-SUB_THUMB = 45
-SUB_FINGER = 90
+STRAIGHT_THUMB = 68000
+STRAIGHT_FINGER = 101100
+BEND_THUMB = 98000
+BEND_FINGER = 250000
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -39,12 +37,13 @@ while True:
     
     thumbADC = mcp.read_adc(0)
     thumbV = thumbADC * VCC / 1023
-    thumbR = RDIV_THUMB * VCC / max(thumbV - 1, 0.001)
+    thumbR = RDIV_THUMB * (VCC / max(thumbV - 1, 0.001))
+    thumbR = min(BEND_THUMB, max(thumbR, STRAIGHT_THUMB))
     # print("Thumb:")
     # print("  " + str(thumbR) + " ohms")
-    thumbAngle = (thumbR / (BEND_THUMB - STRAIGHT_THUMB) * 90) - SUB_THUMB
+    thumbAngle = ((thumbR - STRAIGHT_THUMB) / (BEND_THUMB - STRAIGHT_THUMB)) * 90
     # print("  " + str(thumbAngle) + " degrees")
-    flex = int(min((thumbAngle - 187) / 200, 1) * 10)
+    flex = int((thumbAngle / 90) * 10)
     raws.append(str(flex))
     ratios.append("  =" + ("=" * flex))
     
@@ -52,11 +51,12 @@ while True:
         fingerADC = mcp.read_adc(i + 1)
         fingerV = fingerADC * VCC / 1023
         fingerR = RDIV_FINGER * VCC / max(fingerV - 1, 0.001)
+        fingerR = min(BEND_FINGER, max(fingerR, STRAIGHT_FINGER))
         # print("Finger " + str(i + 1) + ":")
         # print("  " + str(fingerR) + " ohms")
-        fingerAngle = (fingerR / (BEND_FINGER - STRAIGHT_FINGER) * 90) - SUB_FINGER
+        fingerAngle = ((fingerR - STRAIGHT_FINGER) / (BEND_FINGER - STRAIGHT_FINGER)) * 90
         # print("  " + str(fingerAngle) + " degrees")
-        flex = int(min((fingerAngle - 400) / 1000, 1) * 10)
+        flex = int((fingerAngle / 90) * 10)
         raws.append(str(flex))
         ratios.append("  =" + ("=" * flex))
     
@@ -65,7 +65,7 @@ while True:
         print("  " + labels[i + 1] + ratios[i + 1])
     
     print("")
-
+    
     x = str(int(lsm.getXRotation()))
     y = str(int(lsm.getYRotation()))
     z = str(int(lsm.getZRotation()))
